5711af08536a4f62f6ddefa55a00053a0139a38e
justincxu <justinxu@berkeley.edu>
2022-04-26 18:49:29 -0700

    styledd

--- src/Components/Author.js
+++ src/Components/Author.js
@@ -13,17 +13,18 @@ function Author() {
 
 return ( 
 <>
-     <div class="text-center text-5xl mt-40 font-bold">
+
+     <div class="text-center text-5xl mt-36 font-bold">
           BearWear
      </div>
-          <div class="text-center">
+          <div class="text-center text-l">
                digitialize your closet
           </div>
           <div class="flex flex-col justify-center items-center mt-8">    
           <img class= "object-scale-down h-48 w-96" src={bearlogo}></img>
-     <div class="space-x-20 space-y-30 border-solid" >
-          <Link class="space-x-30" to={"/SignIn"}>SignIn</Link>
-          <Link class="space-x-30" to={"/SignUp"}>SignUp</Link>
+     <div class="space-x-24 space-y-30 border-solid m-10" >
+          <Link class="space-x-30 mt-2 w-48 lg:w-3/4 p-2 border border-6 border-blue-800 rounded-3xl text-center hover:bg-sky-700 bg-sky-200" to={"/SignIn"}>Sign In</Link>
+          <Link class="space-x-30 mt-2 w-48 lg:w-3/4 p-2 border border-6 border-blue-800 rounded-3xl text-center hover:bg-sky-700 bg-sky-200" to={"/SignUp"}>Sign Up</Link>
      </div>
 </div>
 </>
